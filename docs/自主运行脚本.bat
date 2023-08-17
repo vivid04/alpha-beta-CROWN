a0ae0f74bba247325d0a12a76dc00caa0bb13c2b
vivid04 <shaoconghan@163.com>
2023-08-17 23:20:01 +0800

    自主运行脚本添加

--- docs/自主运行脚本.bat
+++ docs/自主运行脚本.bat
@@ -1,13 +1,4 @@
-
-for %m in ("rlm","fsb","random","sr") do 
-
-
-
-
-
-cd "D:\wordspace\python\alpha-beta-CROWN\complete_verifier"
-d:
-set cfgName=mnist_6_100_bab
+g`"et cfgName=mnist_6_100_bab
 set timeout=40
 set endID=100
 
@@ -23,7 +14,7 @@ for %e in (0.01,0.02,0.03) do  cmd /C "C:\ProgramData\Anaconda3\envs\nnv\python.
 set method=random
 for %e in (0.01,0.02,0.03) do  cmd /C "C:\ProgramData\Anaconda3\envs\nnv\python.exe  robustness_verifier.py --config exp_configs/%cfgName%_%method%.yaml --branching_method %method% --epsilon %e --timeout %timeout% --end %endID% >>rec%cfgName%_%method%_eps_%e.txt" 
 
-
+https://github.com/vivid04/alpha-beta-CROWN
 
 
 
